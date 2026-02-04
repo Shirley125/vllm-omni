491b684bf04cbec476994de95634b980fd5641aa
CHEN <116010019@link.cuhk.edu.cn>
2026-02-04 13:05:16 +0800

    async schedule
    Signed-off-by: CHEN <116010019@link.cuhk.edu.cn>

--- vllm_omni/patch.py
+++ vllm_omni/patch.py
@@ -9,6 +9,7 @@ from vllm.v1.engine import EngineCoreOutput as _OriginalEngineCoreOutput
 from vllm.v1.engine import EngineCoreOutputs as _OriginalEngineCoreOutputs
 from vllm.v1.engine import EngineCoreRequest as _OriginalEngineCoreRequest
 from vllm.v1.request import Request as _OriginalRequest
+from vllm.v1.request import RequestStatus
 
 import vllm_omni.logger  # noqa: F401
 from vllm_omni.engine import OmniEngineCoreOutput, OmniEngineCoreOutputs, OmniEngineCoreRequest
